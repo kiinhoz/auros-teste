7110f13fa6a1f6d92c29ddf1b704b4bb8781b533
Isaac Barreto <kinroshima2@gmail.com>
2025-07-21 12:06:29 -0300

    complementos

---
+++ products.js
@@ -0,0 +1,162 @@
+const products = [
+    {
+        id: 1,
+        title: "Descrição",
+        price: "Valor",
+        description: "Descrição.",
+        colors: [
+            {
+                name: "flamingo",
+                mainImage: "images/flamingo (5).jpg",
+                thumbnails: [
+                    "images/flamingo (1).jpg",
+                    "images/flamingo (2).jpg",
+                    "images/flamingo (3).jpg"
+                ],
+                sizes: ["PP","P", "M", "G", "GG"]
+            },
+            {
+                name: "Yellow",
+                mainImage: "images/flamingo (6).jpg",
+                thumbnails: [
+                    "images/flamingo (7).jpg",
+                    "images/flamingo (9).jpg",
+                    "images/flamingo (10).jpg"
+                ],
+                sizes: ["PP","P", "M", "G", "GG"]
+            },
+            {
+                name: "Red",
+                mainImage: "images/flamingo (11).jpg",
+                thumbnails: [
+                    "images/flamingo (12).jpg",
+                    "images/flamingo (13).jpg",
+                    "images/flamingo (14).jpg"
+                ],
+                sizes: ["PP","P", "M", "G", "GG"]
+            },
+            {
+                name: "White",
+                mainImage: "images/flamingo (11).jpg",
+                thumbnails: [
+                    "images/flamingo (12).jpg",
+                    "images/flamingo (13).jpg",
+                    "images/flamingo (14).jpg"
+                ],
+                sizes: ["S", "L"]
+            },
+            {
+                name: "Maroon",
+                mainImage: "images/flamingo (11).jpg",
+                thumbnails: [
+                    "images/flamingo (12).jpg",
+                    "images/flamingo (13).jpg",
+                    "images/flamingo (14).jpg"
+                ],
+                sizes: ["M", "L", "XL"]
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Descrição",
+        price: "Valor",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam laborum aut explicabo molestias voluptas, eum nulla doloremque laboriosam. Accusantium omnis illum nulla architecto doloribus.",
+        colors: [
+            {
+                name: "Blue",
+                mainImage: "images/vermelho (1).jpg",
+                thumbnails: [
+                    "images/vermelho (2).jpg",
+                    "images/vermelho (3).jpg",
+                    "images/vermelho (4).jpg"
+                ],
+                sizes: ["M", "L", "XL"]
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: "Descrição",
+        price: "Valor",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam laborum aut explicabo molestias voluptas, eum nulla doloremque laboriosam. Accusantium omnis illum nulla architecto doloribus.",
+        colors: [
+            {
+                name: "Grey",
+                mainImage: "images/preto (1).jpg",
+                thumbnails: [],
+                sizes: ["PP","P", "M", "G", "GG"]
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: "Descrição",
+        price: "Valor",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam laborum aut explicabo molestias voluptas, eum nulla doloremque laboriosam. Accusantium omnis illum nulla architecto doloribus.",
+        colors: [
+            {
+                name: "Green",
+                mainImage: "images/20220306_140223.jpg",
+                thumbnails: [],
+                sizes: ["PP","P", "M", "G", "GG"]
+            }
+        ]
+    },
+    {
+        id: 5,
+        title: "Men Western Style Denim Shirt",
+        price: "$147",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam laborum aut explicabo molestias voluptas, eum nulla doloremque laboriosam. Accusantium omnis illum nulla architecto doloribus.",
+        colors: [
+            {
+                name: "Black",
+                mainImage: "images/20220401_145921.jpg",
+                thumbnails: [],
+                sizes: ["PP","P", "M", "G", "GG"]
+            }
+        ]
+    },
+    {
+        id: 6,
+        title: "Descrição",
+        price: "Valor",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam laborum aut explicabo molestias voluptas, eum nulla doloremque laboriosam. Accusantium omnis illum nulla architecto doloribus.",
+        colors: [
+            {
+                name: "Brown",
+                mainImage: "images/20220414_155326.jpg",
+                thumbnails: [],
+                sizes: ["PP","P", "M", "G", "GG"]
+            }
+        ]
+    },
+    {
+        id: 7,
+        title: "Descrição",
+        price: "Valor",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam laborum aut explicabo molestias voluptas, eum nulla doloremque laboriosam. Accusantium omnis illum nulla architecto doloribus.",
+        colors: [
+            {
+                name: "Blue",
+                mainImage: "images/20220419_180403.jpg",
+                thumbnails: [],
+                sizes: ["PP","P", "M", "G", "GG"]
+            }
+        ]
+    },
+    {
+        id: 8,
+        title: "Descrição",
+        price: "Valor",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam laborum aut explicabo molestias voluptas, eum nulla doloremque laboriosam. Accusantium omnis illum nulla architecto doloribus.",
+        colors: [
+            {
+                name: "Green",
+                mainImage: "images/20220306_152154.jpg",
+                thumbnails: [],
+                sizes: ["PP","P", "M", "G", "GG"]
+            }
+        ]
+    }
+];
